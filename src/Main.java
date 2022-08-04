cd3974e22227dbf3af283152dfc9c2e7195563fc
jkajken <kenasel2@gmail.com>
2022-08-04 23:32:48 +0600

    changes

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        method(20, -20);
-        method(18, -10);
-        method(10, 36);
-        method(45, 41);
-        method(21, -3);
+        System.out.println( isWeatherOkay(20, -20));
+        System.out.println( isWeatherOkay(18, -10));
+        System.out.println(isWeatherOkay(10, 36));
+        System.out.println(isWeatherOkay(45, 41));
+        System.out.println( isWeatherOkay(21, -3));
     }
 
-    public static void method(int age, int temperature) {
+    public static String isWeatherOkay (int age, int temperature) {
+
+
 
         if (age >= 20 && age <= 45 && temperature >= -20 && temperature <= 30)
-            System.out.println("You can go for a walk");
+            return "You can go for a walk" ;
 
         else if
         (age < 20 && temperature >= 0 && temperature <= 28)
 
-            System.out.println("You can go for a walk");
+            return "Stay at home" ;
 
         else if (age > 45 && temperature >= -10 && temperature <= 25)
-            System.out.println("You can go for a walk");
+            return "You can go for a walk" ;
         else
-            System.out.println("Stay at home");
-
+            return "Stay at home" ;
 
     }
 }
